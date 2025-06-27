453cf21e941ba1d44c8d5500ee8f85a4f213cff5
sadikatanisha <sadikatanisha@gmail.com>
2025-06-28 00:52:34 +0600

    eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     domains: ["images.pexels.com", "images.unsplash.com", "pbs.twimg.com"],
     remotePatterns: [
